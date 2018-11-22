e6ecd0b8ec4100adf4b3094afd75bb44e9e17b5c
Hiram <hiram@upscalemedia.mx>
2018-11-21 19:38:21 -0600

    Estilos Sabrina

--- common-js/scripts.js
+++ common-js/scripts.js
@@ -1,23 +1,14 @@
-
-(function ($) {
-
-    "use strict";
-	
-	
-
+(function($) {
+	"use strict";
 	// LINE PROGRESS BAR
 	enableLineProgress();
-	
 	// RADIAL PROGRESS BAR
 	enableRadialProgress();
-	
 	// ACCORDIAN
 	panelAccordian();
-
-	$(window).on('load', function(){
-		
+	$(window).on('load', function() {
 		// ISOTOPE PORTFOLIO WITH FILTER
-		if(isExists('.portfolioContainer')){
+		if (isExists('.portfolioContainer')) {
 			var $container = $('.portfolioContainer');
 			$container.isotope({
 				filter: '*',
@@ -27,11 +18,9 @@
 					queue: false
 				}
 			});
-		 
-			$('.portfolioFilter a').click(function(){
+			$('.portfolioFilter a').click(function() {
 				$('.portfolioFilter .current').removeClass('current');
 				$(this).addClass('current');
-		 
 				var selector = $(this).attr('data-filter');
 				$container.isotope({
 					filter: selector,
@@ -40,128 +29,115 @@
 						easing: 'linear',
 						queue: false
 					}
-				 });
-				 return false;
-			}); 
+				});
+				return false;
+			});
 		}
-	
 	});
-	
-	
-	$('a[href="#"]').on('click', function(event){
+	$('a[href="#"]').on('click', function(event) {
 		return;
 	});
-	
-	
-	if ( $.isFunction($.fn.fluidbox) ) {
+	if ($.isFunction($.fn.fluidbox)) {
 		$('a').fluidbox();
 	}
-	
 	var countCounterUp = 0;
-	
-	var a = 0 ;
-	
+	var a = 0;
 	countCounterUp = enableCounterUp(countCounterUp);
-	
-	$(window).on('scroll', function(){
-		
+	$(window).on('scroll', function() {
 		countCounterUp = enableCounterUp(countCounterUp);
-	
 	});
-	
-	
 })(jQuery);
 
-function panelAccordian(){
-	
+function panelAccordian() {
 	var panelTitle = $('.panel-title');
-	panelTitle.on('click', function(){
+	panelTitle.on('click', function() {
 		$('.panel-title').removeClass('active');
 		$(this).toggleClass('active');
-		
 	});
-	
 }
 
-function enableRadialProgress(){
-	
-	$(".radial-progress").each(function(){
+function enableRadialProgress() {
+	$(".radial-progress").each(function() {
 		var $this = $(this),
 			progPercent = $this.data('prog-percent');
-			
 		var bar = new ProgressBar.Circle(this, {
 			color: '#aaa',
 			strokeWidth: 3,
 			trailWidth: 1,
 			easing: 'easeInOut',
 			duration: 1400,
-			text: {
-				
+			text: {},
+			from: {
+				color: '#aaa',
+				width: 1
 			},
-			from: { color: '#aaa', width: 1 },
-			to: { color: '#FEAE01', width: 3 },
+			to: {
+				color: '#ff0101',
+				width: 3
+			},
 			// Set default step function for all animate calls
 			step: function(state, circle) {
 				circle.path.setAttribute('stroke', state.color);
 				circle.path.setAttribute('stroke-width', state.width);
-
 				var value = Math.round(circle.value() * 100);
 				if (value === 0) {
 					circle.setText('');
 				} else {
 					circle.setText(value);
 				}
-
 			}
 		});
-		
-		$(this).waypoint(function(){
-		   bar.animate(progPercent);  
-		},{offset: "90%"})
-		
+		$(this).waypoint(function() {
+			bar.animate(progPercent);
+		}, {
+			offset: "90%"
+		})
 	});
 }
 
-function enableLineProgress(){
-	
-	$(".line-progress").each(function(){
+function enableLineProgress() {
+	$(".line-progress").each(function() {
 		var $this = $(this),
 			progPercent = $this.data('prog-percent');
-			
 		var bar = new ProgressBar.Line(this, {
 			strokeWidth: 1,
 			easing: 'easeInOut',
 			duration: 1400,
-			color: '#FEAE01',
+			color: '#ff0101',
 			trailColor: '#eee',
 			trailWidth: 1,
-			svgStyle: {width: '100%', height: '100%'},
-			text: {
-				style: {
-					
-				},
+			svgStyle: {
+				width: '100%',
+				height: '100%'
 			},
-			from: {color: '#FFEA82'},
-			to: {color: '#ED6A5A'},
+			text: {
+				style: {},
+			},
+			from: {
+				color: '#FFEA82'
+			},
+			to: {
+				color: '#ED6A5A'
+			},
 			step: (state, bar) => {
 				bar.setText(Math.round(bar.value() * 100) + ' %');
 			}
 		});
-		
-		$(this).waypoint(function(){
-		   bar.animate(progPercent);  
-		},{offset: "90%"})
-		
+		$(this).waypoint(function() {
+			bar.animate(progPercent);
+		}, {
+			offset: "90%"
+		})
 	});
 }
 
-function enableCounterUp(a){
-	
+function enableCounterUp(a) {
 	var counterElement;
-	
-	if(isExists('#counter')){ counterElement = $('#counter'); }
-	else{ return; }
-		
+	if (isExists('#counter')) {
+		counterElement = $('#counter');
+	} else {
+		return;
+	}
 	var oTop = $('#counter').offset().top - window.innerHeight;
 	if (a == 0 && $(window).scrollTop() > oTop) {
 		$('.counter-value').each(function() {
@@ -172,8 +148,7 @@ function enableCounterUp(a){
 				countNum: $this.text()
 			}).animate({
 				countNum: countTo
-			},{
-
+			}, {
 				duration: countDuration,
 				easing: 'swing',
 				step: function() {
@@ -182,17 +157,15 @@ function enableCounterUp(a){
 				complete: function() {
 					$this.text(this.countNum);
 				}
-
 			});
 		});
 		a = 1;
 	}
-
 	return a;
 }
 
-function isExists(elem){
-	if ($(elem).length > 0) { 
+function isExists(elem) {
+	if ($(elem).length > 0) {
 		return true;
 	}
 	return false;
